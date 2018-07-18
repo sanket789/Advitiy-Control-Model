69cedee10989862115683215d033ee727c035b01
Sanket Chirame <sanketchirame12@gmail.com>
2018-07-18 23:13:52 +0530

    removed mistakes in getLLA
    The sgp_output has 4 columns with first being time instant. This was not considered in earlier calculation giving wrong lat and lon.

--- getLLA.py
+++ getLLA.py
@@ -1,20 +1,30 @@
 import numpy as np
 import frames as fs
 from constants_1U import R_EARTH
+import math
+import matplotlib.pyplot as plt
+
 m_sgp_output_i = np.genfromtxt('sgp_output.csv', delimiter=",")
 N = m_sgp_output_i.shape[0]
 m_sgp_ecef = np.zeros([N,4])
 m_LLA = np.zeros([N,4])
 
 for k in range(0,N):
-	m_sgp_ecef[k,0] = m_sgp_output_i[k,0]
-	m_LLA[k,0] = m_sgp_output_i[k,0]
-	#get position in ecef
-	m_sgp_ecef[k,1:4] = fs.ecif2ecef(m_sgp_output_i[k,1:4],m_sgp_output_i[k,0])
-	#get latitude and longitude and altitude
-	m_LLA[k,1:3] = fs.latlon(m_sgp_ecef[k,:].copy())
-	m_LLA[k,3] = np.linalg.norm(m_sgp_ecef[k,:].copy()) - R_EARTH	#in meters
+	if math.fmod(k,N/100) == 0:
+		print int(100.*k/N) 
 
+	v_i = m_sgp_output_i[k,1:4]
+	time = m_sgp_output_i[k,0]
+	#get position in ecef
+	v_ecef = fs.ecif2ecef(v_i,time)
+	#get latitude and longitude and altitude
+	v_latlon = fs.latlon(v_ecef.copy())
+	alt = np.linalg.norm(v_i.copy()) - R_EARTH	#in meters
+	
+	m_sgp_ecef[k,0] = time
+	m_LLA[k,0] = time
+	m_sgp_ecef[k,1:4] = v_ecef
+	m_LLA[k,1:4] = np.append(v_latlon,alt)
 np.savetxt('LLA.csv',m_LLA, delimiter=",")
 print "LLA done"
 
@@ -26,3 +36,6 @@ for k in range(N):
 	m_mag_ecef = fs.ned2ecef(m_mag_ned[k].copy())
 	m_mag_i[k] = fs.ecef2ecif(m_mag_ecef.copy())
 '''
+
+plt.plot(m_sgp_output_i[:,0],m_LLA[:,1])
+plt.show()
