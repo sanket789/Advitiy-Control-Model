36f2c0d9719dc2c8ffab089a9876d03f2d3b639e
Sanket Chirame <sanketchirame12@gmail.com>
2018-07-27 23:42:01 +0530

    Modified ned2ecef function in frames WIP
    Corrected the mistake in earlier code due to intercanged asignment of theta and phi. Also introduced valueerror for 90 degree lat

--- frames.py
+++ frames.py
@@ -144,11 +144,17 @@ def wBIb2wBOb(v_w_BI_b,v_q_BO,v_w_IO_o):
 
 def ned2ecef(v,lat,lon):
 	#rotate vector from North-East-Down frame to ecef
-	# lat should be -90 to 90
-	# lon should be 0 to 360
-	
-	phi = -lat + 90. #in degree, polar angle (co-latitude)
-	theta = lon #in degree, azimuthal angle
+	#Input:
+	#	lat: latittude in degrees ranges from -90 to 90
+	#	lon: longitude in degrees ranges from (-180,180]	 
+	if lat == 90 or lat == -90:
+		raise ValueError('Latittude value +/-90 occured. NED frame is not defined at north and south pole !!')
+	theta = -lat + 90. #in degree, polar angle (co-latitude)
+
+	if lon<0:
+		phi = 360. - abs(lon)
+	else:
+		phi = lon #in degree, azimuthal angle
 	theta = radians(theta)
 	phi = radians(phi)
 
@@ -156,7 +162,6 @@ def ned2ecef(v,lat,lon):
 						[	-cos(theta)*sin(phi),	cos(phi),	-sin(theta)*sin(phi)],
 						[	sin(theta),	0.0,	-cos(theta)]])
 	
-
 	y = np.dot(m_DCM_n2e,v)
 
 	return y
